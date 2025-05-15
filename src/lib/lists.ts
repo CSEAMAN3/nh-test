720d3d36f9c9221a7cd90f147872df8db6398967
Chris Seaman <christopherseaman1983@gmail.com>
2025-05-15 07:21:26 +0100

    update

--- src/lib/lists.ts
+++ src/lib/lists.ts
@@ -36,3 +36,54 @@ export const reviews = [
     img: "tman.webp",
   },
 ]
+
+export const faqQuestions = [
+  {
+    id: 1,
+    category: "General Info",
+    question: "What age groups do you teach?",
+    answer: "We teach children as young as 6 years and up to the age of 14 years.",
+  },
+  {
+    id: 2,
+    category: "General Info",
+    question: "Can my child try a session before making a commitment?",
+    answer: "Yes, we can offer you first session on a one session basis. After that you would be required to make a block booking for a minimum of 4 weeks",
+  },
+  {
+    id: 3,
+    category: "General Info",
+    question: "How do I book a session?",
+    answer: "There are various ways to book a session. The easiest way is to click the book your first session button located throughout the website. An email will then be sent to confirm your appointment. Alternativly you can email us on hello@nhfc.co.uk or call us on 01603 783821",
+  },
+  {
+    id: 4,
+    category: "General Info",
+    question: "Where are the sessions held?",
+    answer: "The sessions are held at Mulbarton Football Club. Full address here.",
+  },
+  {
+    id: 5,
+    category: "Coaching & Sessions",
+    question: "What happens during a typical session?",
+    answer: "During a typical session we do x, y and z. At the edd of every session we evalute and set activities for the child to complete throughout the week to develop their abiltiy.",
+  },
+  {
+    id: 6,
+    category: "Coaching & Sessions",
+    question: "Do you offer one to one or group sessions?",
+    answer: "We provide one to one or one to two coaching session as we beleive this is the fasted way for children to develop their ability. Our one to one sessions are designed around building confidence, improving technique and learning new skills. Our one to two sessions are designed to improve decision making, improve tactical awareness and simulate match realistic scenarios.",
+  },
+  {
+    id: 7,
+    category: "Coaching & Sessions",
+    question: "How do you support shy / nervous children?",
+    answer: "For children that are shy or nervous we recommend starting with one to one sessions. Our coaching ethos is centered around building confidence",
+  },
+  {
+    id: 8,
+    category: "Coaching & Sessions",
+    question: "?",
+    answer: "",
+  },
+]
